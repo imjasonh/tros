a7e55cdfe9d322c2b22e96678202fef638de7640
Jason Hall <imjasonh@gmail.com>
2014-08-05 10:45:13 -0400

    initial

---
+++ tros.go
@@ -0,0 +1,75 @@
+package tros
+
+import (
+	"fmt"
+	"reflect"
+	"sort"
+)
+
+/*
+func main() {
+	in := []struct {
+		A, B string
+	}{{"a", "c"}, {"b", "b"}, {"c", "a"}}
+	Sort(in, "A")
+	fmt.Println(in)
+
+	Sort(in, "B")
+	fmt.Println(in)
+}*/
+
+// Sort sorts a slice of structs based on the values of the structs' fields with the given field name fn
+func Sort(i interface{}, fn string) error {
+	sval := reflect.ValueOf(i)
+	if sval.Kind() != reflect.Slice {
+		return fmt.Errorf("non-slice interface, got %q", sval.Kind())
+	}
+	s := sortable{sval, fn, sval.Len(), nil}
+	sort.Sort(sort.Interface(s))
+	return s.err
+}
+
+type sortable struct {
+	sval reflect.Value
+	fn   string
+	len  int
+	err  error
+}
+
+func (s sortable) Len() int { return s.len }
+
+func (s sortable) Swap(i, j int) {
+	if s.err != nil {
+		return
+	}
+	a, b := s.sval.Index(i), s.sval.Index(j)
+	tmp := reflect.New(a.Type()).Elem()
+	tmp.Set(a)
+	a.Set(b)
+	b.Set(tmp)
+}
+
+func (s sortable) Less(i, j int) bool {
+	a, b := s.sval.Index(i), s.sval.Index(j)
+	af, bf := a.FieldByName(s.fn), b.FieldByName(s.fn)
+	afk, bfk := af.Kind(), bf.Kind()
+	if afk != bfk {
+		s.err = fmt.Errorf("unmatched kinds, %q vs %q", afk, bfk)
+		return false
+	}
+	switch afk {
+	case reflect.Bool:
+		return !af.Bool() && bf.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return af.Int() < bf.Int()
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return af.Uint() < bf.Uint()
+	case reflect.Float32, reflect.Float64:
+		return af.Float() < bf.Float()
+	case reflect.String:
+		return af.String() < bf.String()
+	default:
+		s.err = fmt.Errorf("unsupported kind %q", afk)
+		return false
+	}
+}
---
+++ tros_test.go
@@ -0,0 +1,36 @@
+package tros
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSort(t *testing.T) {
+	type s struct {
+		A, B string
+	}
+	for _, c := range []struct {
+		ss  []s
+		fn  string
+		exp []s
+		err error
+	}{{
+		[]s{{"a", "c"}, {"b", "b"}, {"c", "a"}},
+		"B",
+		[]s{{"c", "a"}, {"b", "b"}, {"a", "c"}},
+		nil,
+	}, {
+		[]s{{"a", "c"}, {"b", "b"}, {"c", "a"}},
+		"A",
+		[]s{{"a", "c"}, {"b", "b"}, {"c", "a"}},
+		nil,
+	}} {
+		if err := Sort(c.ss, c.fn); err != c.err {
+			t.Errorf("unexpected error: %v", err)
+		}
+		if c.err != nil && !reflect.DeepEqual(c.ss, c.exp) {
+			t.Errorf("unexpected result\n got %v\nwant %v", c.ss, c.exp)
+		}
+	}
+
+}
